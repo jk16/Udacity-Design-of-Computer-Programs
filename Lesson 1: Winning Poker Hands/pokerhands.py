1afc57865da7fb2d0442ab36388fc457a8beea9d
jk16 <dude16kh@gmail.com>
2016-02-09 12:24:13 -0500

    added test function and tests

--- Lesson 1: Winning Poker Hands/pokerhands.py
+++ Lesson 1: Winning Poker Hands/pokerhands.py
@@ -1,6 +1,20 @@
 def poker(hands):
-    "Return the best hand: poker[hand,...] => hand"
+    """Return the best hand: poker[hand,...] => hand"""
     return max(hands, key=hand_rank)
 
 def hand_rank(hand):
-    return 
+    return ???
+
+
+def test():
+    """Test cases for the functions in poker program"""
+
+
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+
+    assert poker([sf, fk, fh]) == sf
+    reeturn "tests pass"
+
+print (test())
